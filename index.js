83ff0392c2167bfcd79abd993a8a3003aa135e64
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2021-01-24 19:44:37 +0100

    view counter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express"),
     bodyParser = require("body-parser"),
-    websocket = require('ws');
+    websocket = require('ws'),
+    package = require('./package')
 
 const app = express(),
     wss = new websocket.Server({ port: 5502 }),
@@ -8,44 +9,105 @@ const app = express(),
         _count: 0,
         _last: 0,
         _list: {},
-        open(ws) {
-            this._count++
+        _ips: {},
+        open(ws, ip) {
+
             let id = (new Date()).getTime().toString(36)
+
             ws.id = id
+            ws.visibility = true
+            ws.ip = ip
+
+            if(typeof this._ips[ip] == 'undefined') this._ips[ip] = []
+
+            if(this._ips[ip].length >= 2) {
+                ws.close()
+                log(ip, `too many concurrent connections`)
+            }
+
             this._list[id] = ws
-            this.broadcast()
+            this._ips[ip].push(id)
+            this.count(1)
         },
         close(ws) {
-            this._count--
+            if (ws.visibility) this.count(-1)
             delete this._list[ws.id]
+
+            let index = this._ips[ws.ip].indexOf(ws.id)
+            if(index != -1) this._ips[ws.ip].splice(index, 1)
+        },
+        count(n) {
+            this._count += n
             this.broadcast()
         },
-        count() {
+        counter() {
             return this._count
+        },
+        visibility(ws, visible) {
+            if (ws.visibility && !visible) {
+                this.count(-1)
+                console.log(new Date().toLocaleTimeString('pl-PL'), '-', Sockets.counter(), ws.id)
+            } else if (!ws.visibility && visible) {
+                this.count(1)
+                console.log(new Date().toLocaleTimeString('pl-PL'), '+', Sockets.counter(), ws.id)
+            }
+            ws.visibility = visible
         },
         broadcast() {
             if (Math.abs(this._count - this._last) > 0)
                 for (let id in this._list) {
-                    this._list[id].send(this.count())
+                    sendObject(this._list[id], {
+                        type: 'count',
+                        count: this._count
+                    })
                 }
         }
     }
 
+function sendObject(ws, object) {
+    return ws.send(JSON.stringify(object))
+}
+
+function log(...message){
+    console.log(new Date().toLocaleTimeString('pl-PL'), ...message)
+}
+
 wss.on('connection', function connection(ws, req) {
-    Sockets.open(ws)
+    Sockets.open(ws, req.headers['x-real-ip'])
 
-    console.log('+', Sockets.count(), ws.id, req.headers['x-real-ip'])
+    log(new Date().toLocaleTimeString('pl-PL'), '+', Sockets.counter(), ws.id, req.headers['x-real-ip'])
 
-    // ws.on('message', function incoming(message) {
-    //     console.log('received: ', message);
-    // });
+    sendObject(ws, {
+        type: 'sync.begin'
+    })
 
     ws.on('close', function () {
         Sockets.close(ws)
-        console.log('-', Sockets.count(), ws.id, req.headers['x-real-ip'])
+        log(new Date().toLocaleTimeString('pl-PL'), '-', Sockets.counter(), ws.id, req.headers['x-real-ip'])
     })
 
-    ws.send(Sockets.count());
+    ws.on('message', function (e) {
+        let data = JSON.parse(e)
+
+        if (data.type == 'sync.received') {
+            sendObject(ws, {
+                type: 'sync.end',
+                time: Date.now()
+            })
+        } else if (data.type == 'visibility') {
+            Sockets.visibility(ws, data.visible)
+        }
+    })
+
+    sendObject(ws, {
+        type: 'count',
+        count: Sockets.counter()
+    })
+
+    sendObject(ws, {
+        type: 'version',
+        version: package.version
+    })
 });
 
 app.use(bodyParser.json());
